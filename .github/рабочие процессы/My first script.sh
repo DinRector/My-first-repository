d2ce415346d1cc6fc2b8b14245a2fe694f854bb2
DinRector <121034479+DinRector@users.noreply.github.com>
2022-12-19 16:08:38 +0300

    din11.sh

---
+++ .github/рабочие процессы/My first script.sh
@@ -0,0 +1,13 @@
+Echo Hello!
+Echo My first script for termux.
+Echo.           
+Echo $$$$$$$$$$$$$$$$$$$$$$$$$
+Echo $$---$$$$---$---$---$$$$$
+Echo $$$----$$$$$$$-------$-$$
+Echo $----$$$$$$$---$$$$$$--$$
+Echo $$-----$$-$---$$$$$$$$-$$
+Echo $$$$$$$$$$$$$$$$$$$$$$$$$
+Echo.       
+Echo This picture. :)
+Echo Bye (By DinRector).
+Echo.           
